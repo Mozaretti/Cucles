dabc9ae4583f079145621fb45c8788fb6827c022
Eвгений <mocard778@gmail.com>
2023-02-06 17:00:14 +0700

    начало

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+    public static void task1(){
+        System.out.println("Задача 1");
+        for (int i = 0; i < 10 ; i = i + 1){
+            System.out.println(" Итерация цикла " + i);
+        }
+        System.out.println();
+    }
+    public static void task2(){
+        System.out.println("Задача 2");
+        for (int i = 10; i > 0 ; i= i - 1){
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+}
